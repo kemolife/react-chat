9a8f237d8535b48e8f3b2b7eb302e51d2757aa39
kemolife <kemolife1990@gmail.com>
2017-10-09 23:44:14 +0300

    #2

--- client/components/App.js
+++ client/components/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import socket from 'socket.io-client';
 
 class App extends React.Component {
+
+	constructor(prop){
+		super(prop);
+		this.state = {
+			socket : socket,
+			name: 'olia'
+		}
+	}
+
+	componentDidMount(){
+
+        this.state.socket.connect().emit('create user')
+			.on('user created', (data) => {
+            this.setState({
+                name: data.username
+            });
+        })
+	}
+
 	render() {
 		return (
-			<h1> Hello World from React</h1>
+			<h1>{this.state.name}</h1>
 		);
 	}
 }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
+import config from './config.json';
 import express from 'express';
-import path from 'path';
+import { Server } from 'http';
+import socket from 'socket.io';
+import Chat from './chat/chat';
+import Logger from 'winston-console-graylog2-logger';
 
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
@@ -11,14 +15,39 @@ let app = express();
 const compiler = webpack(webpackConfig)
 
 app.use(webpackMiddleware(compiler, {
-	hot:true,
-	publicPath: webpackConfig.output.publicPath,
-	noInfo: true 
+    hot:true,
+    publicPath: webpackConfig.output.publicPath,
+    noInfo: true
 }));
 app.use(webpackHotMiddleware(compiler));
 
-app.get('/*', (req,res) => {
-	res.sendFile(path.join(__dirname,'./index.html'));
+let http = Server(app);
+let io = socket(http);
+let port = 3000;
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, () => console.log('running on localhost:3000'));
+http.listen(port, '127.0.0.1', () => console.info('Chat App listening on 127.0.0.1:'+port));
+
+config.logger = config.logger || {};
+
+let graylog2Cnf = config.logger.graylog2 || {};
+
+graylog2Cnf.enable = graylog2Cnf.enable || false;
+graylog2Cnf.host = graylog2Cnf.host || '127.0.0.1';
+graylog2Cnf.port = graylog2Cnf.port || '12201';
+graylog2Cnf.facility = graylog2Cnf.facility || 'My NodeJS App';
+
+let log = new Logger({
+    enable: !!config.logger.enable,
+    graylog2: {
+        enable: graylog2Cnf.enable,
+        host: graylog2Cnf.host,
+        port: graylog2Cnf.port,
+        facility: graylog2Cnf.facility
+    }
+});
+
+new Chat(config, io, log);
